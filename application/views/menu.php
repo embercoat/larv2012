ff6da7781652b1dd1c67a7ae11ba2ecf3c1ead8e
stefan <stefan@scripter.se>
2011-11-13 22:05:10 +0100

    Site menu is now dynamic. /Stefan

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,12 +1,17 @@
-<ul id="mainMenu" style="float: left;">
-	<li><a href="/">Hem</a></li>
-	<li><a href="/info/">Info</a></li>
-	<li><a href="/katalog/">Katalog</a></li>
-	<li><a href="/student/">Student</a></li>
-	<li><a href="/foretag/">Företag</a></li>
-	<li><a href="/kontakt/">Kontakt</a></li>
-</ul>
-<ul style="float: right;">
-	<li style="display: inline; margin-right: 20px;"><a href="/admin/login/redirect/_">Logga In</a></li>
-	<li style="display: inline;"><a href="/register/">Registrera</a></li>
-</ul>
+<?php
+
+$curPage = Request::$current->controller();
+
+$menu = array(
+	array("controller" => "welcome", "url" => "/",         "title" => "Start",                        "text" => "Start"),
+	array("controller" => "omlarv",  "url" => "/omlarv/",  "title" => "Information om LARV",          "text" => "Om LARV"),
+	array("controller" => "foretag", "url" => "/foretag/", "title" => "Information för företag",      "text" => "Företag"),
+	array("controller" => "student", "url" => "/student/", "title" => "Kontaktinformation till LARV", "text" => "Studenter"),
+	array("controller" => "kontakt", "url" => "/kontakt/", "title" => "Information för studenter",    "text" => "Kontakt"),
+);
+
+foreach($menu as $m) {
+	$class = ($curPage == $m["controller"]) ? ' class="active"' : '';
+	echo "<li><a{$class} href=\"{$m["url"]}\" title=\"{$m["title"]}\"><span>{$m["text"]}</span></a></li>\n";
+}
+
